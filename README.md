a3f7aa65664c68d6e46405a0652391f05625b983
Govinda <govi2010@gmail.com>
2020-04-30 11:02:04 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# angular-9-boilerplate
-boilerplate for angular project 
+#Angular Universal v9
+
+## Installation
+
+```bash
+npm install
+```
+
+## Live Universal Development
+
+```bash
+npm run dev:ssr
+```
+
+Open [http://localhost:4200](http://localhost:4200)
+
+## Angular Universal Prerendering
+
+```bash
+npm run prerender
+```
+
+Everything should be generated in your `/dist/angular-universa-v9/browser/` folder. Or utilize a tool like `http-server` to spin up a quick server to see it live.
+
+## Angular Universal Dynamic SSR
+
+```bash
+ npm run build:ssr && npm run serve:ssr
+```
+
+Open [http://localhost:4000](http://localhost:4000)
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBpptwGwbBY7sWAAmWQ6Xp4qtu1iZ4r4Ko',
+    authDomain: 'vue-blog-d1af9.firebaseapp.com',
+    databaseURL: 'https://vue-blog-d1af9.firebaseio.com',
+    projectId: 'vue-blog-d1af9',
+    storageBucket: 'vue-blog-d1af9.appspot.com',
+    messagingSenderId: '157912072150',
+    appId: '1:157912072150:web:6726a21ceca7f01e58ee77'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
